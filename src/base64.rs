15a2c4e7dbb27fefadea0cd2aecf524e69462c1f
jejbuitenhuis <jorambuitenhuis@gmail.com>
2022-04-08 16:18:02 +0200

    Add base64 encoding and decoding for database id's

---
+++ src/base64.rs
@@ -0,0 +1,60 @@
+pub struct Base64 {}
+
+impl Base64 {
+	const ALPHABET: &'static [u8] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_".as_bytes();
+
+	fn index_of_char(char: char) -> Option<u64> {
+		Base64::ALPHABET.iter()
+			.position(|&c| c == char as u8)
+			.map(|i| i as u64)
+	}
+
+	pub fn encode(num: u64) -> String {
+		// num == 0 doesn't work because of the `while num > 0`
+		if num == 0 {
+			return ( Base64::ALPHABET[0] as char ).to_string();
+		}
+
+		let mut num = num;
+		let mut result = String::new();
+		let mut bit_parts: Vec<u8> = Vec::new();
+
+		while num > 0 {
+			// get the trailing 6 bits
+			// force the cast from u64 to u8 because we know it will fit
+			let bit_part: u8 = (num & 0b111111).try_into()
+				.expect("Error casting u64 to u8 in Base64::encode");
+
+			bit_parts.push(bit_part);
+
+			// remove the trailing 6 bits
+			num = num >> 6;
+		}
+
+		for part in bit_parts.iter() {
+			// get the character corresponding with the 6 bits (the index in
+			// the alphabet)
+			result.push( Base64::ALPHABET[*part as usize] as char );
+		}
+
+		result
+	}
+
+	pub fn decode(string: String) -> Result<u64, String> {
+		let mut result = 0u64;
+
+		// reverse of `Base64::encode`
+		for char in string.chars().rev() {
+			let index = match Base64::index_of_char(char) {
+				Some(c) => c,
+				None => return Err(
+					String::from("Invalid character found in supplied string")
+				),
+			};
+
+			result = (result << 6) + index;
+		}
+
+		Ok(result)
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use warp::Filter;
+use crate::base64::Base64;
 use crate::upload::handle_upload;
 use crate::database::Database;
 use crate::pages::home::HomePage;
 use askama::Template;
 use std::convert::Infallible;
 
+mod base64;
 mod database;
 mod upload;
 mod pages;
@@ -25,6 +27,18 @@ async fn main() {
 	let database = Database::new(database_url).await;
 
 	let endpoints = {
+		let test = warp::path!("test")
+			.and_then(|| async {
+				let encoded = Base64::encode(1234567890);
+				let decoded = Base64::decode( "SLglJB".to_string() ).unwrap();
+
+				Ok::<_, Infallible>(
+					warp::reply::html(
+						format!("<p>{}</p><p>{}</p>", encoded, decoded)
+					)
+				)
+			});
+
 		let home = warp::get()
 			.and( warp::path::end() )
 			.and_then(|| async {
@@ -41,7 +55,9 @@ async fn main() {
 			.and( warp::multipart::form().max_length(max_upload_size) )
 			.and_then(handle_upload);
 
-		home.or(upload)
+		home
+			.or(upload)
+			.or(test)
 	};
 
 	warp::serve(endpoints)
